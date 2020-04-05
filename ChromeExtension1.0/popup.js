27eda31b3a5019e891442d725afa2e9f12ba61f8
Andrew Chen <chen_andrew@berkeley.edu>
2020-04-05 13:47:37 -0700

    ChromeExtension1.0: Create function to check if an article has already been submitted and audited

--- ChromeExtension1.0/popup.js
+++ ChromeExtension1.0/popup.js
@@ -22,15 +22,17 @@ function getURL() {
  */ 
 function checkURL() {
   //Note: this.responseText is generated before this function is called. The responseText is what our server responds to the request with, but converted into a string.
+
   if (this.readyState == 4 && this.status == 200) {
     document.getElementById("result").innerText = "This article has already been submitted.";
   } else if (this.readyState == 4 && this.status == 500) {
     let response = JSON.parse(this.responseText);
-    console.log(response.message);
+    console.log("First time" + response.message);
     if (response.error.localeCompare("Internal Server Error") === 0 
         && response.message.localeCompare("No message available") === 0
         && response.path.localeCompare("/demo-0.0.1-SNAPSHOT/article/submit") === 0) {
           //When submits for the first time, error 500 with specific message
+
       document.getElementById("result").innerText = "Thank you for submitting this article!";  
     }
   } else if (this.readyState == 4) {
@@ -55,6 +57,28 @@ function submitURL(targetURL) {
   xhttp.open("POST", "http://157.230.221.241:8080/demo-0.0.1-SNAPSHOT/article/submit?url=" 
     + targetURL.toString(), true);
   xhttp.send();
+}
+
+
+/**
+ * Checks if an article has been both submitted and audited. 
+ * For example, this function called on
+ * https://www.vox.com/science-and-health/2017/2/16/14622198/doctors-prescribe-opioids-varies-patients-hooked
+ * would return true since it has been audited by public editor.
+ *
+ * @param {string} url The url of the article to be verified.
+ * @returns {boolean} true if the article has been audited, else false.
+ */
+async function verifyAudit(url) {
+  let response = await fetch('http://157.230.221.241:8080/demo-0.0.1-SNAPSHOT/article');
+  let data = await response.json()
+  for (let article of data) {
+    if (article.visData &&article.url.localeCompare(url, {sensitivity: 'case'}) === 0) {
+      console.log(article);
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
