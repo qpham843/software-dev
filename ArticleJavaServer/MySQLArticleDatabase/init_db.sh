d502614c7c9faa09506eff0d03c9e5164661fdc8
Norman Gilmore <git@teamforecast.com>
2020-04-06 22:49:55 -0700

    Specify my.cnf explicitly when calling mysql.

--- ArticleJavaServer/MySQLArticleDatabase/init_db.sh
+++ ArticleJavaServer/MySQLArticleDatabase/init_db.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Set this script as the start command for an ECS Task to initialize an AWS RDS MySQL database.
-cat <<EOF >/root/.my.cnf
+set -e
+cat <<EOF >my.cnf
 [mysql]
 user = ${MYSQL_ROOT_USER}
 password = ${MYSQL_ROOT_PASSWORD}
 EOF
-chmod 0600 /root/.my.cnf
+chmod 0600 my.cnf
 echo "Initializing database '${MYSQL_DATABASE}' on host '${MYSQL_HOST}' with a-users.sql"
-mysql -h ${MYSQL_HOST} ${MYSQL_DATABASE} < /docker-entrypoint-initdb.d/a-users.sql
+mysql --defaults-extra-file=my.cnf -h ${MYSQL_HOST} ${MYSQL_DATABASE} < /docker-entrypoint-initdb.d/a-users.sql
 echo "Initializing database '${MYSQL_DATABASE}' on host '${MYSQL_HOST}' with publiceditor-database-dump.sql"
-mysql -h ${MYSQL_HOST} ${MYSQL_DATABASE} < /docker-entrypoint-initdb.d/publiceditor-database-dump.sql
+mysql --defaults-extra-file=my.cnf -h ${MYSQL_HOST} ${MYSQL_DATABASE} < /docker-entrypoint-initdb.d/publiceditor-database-dump.sql
 echo "Initialization of database '${MYSQL_DATABASE}' on host '${MYSQL_HOST}' complete."
