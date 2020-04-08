ce2af02241b304b392c80e834e78fc03bb6b55ee
Norman Gilmore <git@teamforecast.com>
2020-04-07 18:46:14 -0700

    On Article Funnel DB init, remove user 'MysqlUpdateUser' and create app user based on env.
    Add convenience script mysql.sh for connecting to db.

--- ArticleJavaServer/MySQLArticleDatabase/init_db.sh
+++ ArticleJavaServer/MySQLArticleDatabase/init_db.sh
@@ -11,4 +11,22 @@ echo "Initializing database '${MYSQL_DATABASE}' on host '${MYSQL_HOST}' with a-u
 mysql --defaults-extra-file=my.cnf -h ${MYSQL_HOST} ${MYSQL_DATABASE} < /docker-entrypoint-initdb.d/a-users.sql
 echo "Initializing database '${MYSQL_DATABASE}' on host '${MYSQL_HOST}' with publiceditor-database-dump.sql"
 mysql --defaults-extra-file=my.cnf -h ${MYSQL_HOST} ${MYSQL_DATABASE} < /docker-entrypoint-initdb.d/publiceditor-database-dump.sql
+
+# Drop this test user with known credentials.
+mysql --defaults-extra-file=my.cnf -h ${MYSQL_HOST} ${MYSQL_DATABASE} <<EOF
+use mysql;
+DROP USER 'MysqlUpdateUser'@'*';
+SELECT user, host from user;
+EOF
+
+# Create a new user for the app with least permissions needed.
+mysql --defaults-extra-file=my.cnf -h ${MYSQL_HOST} ${MYSQL_DATABASE} <<EOF
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, LOCK TABLES
+ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+
+SHOW GRANTS FOR '${MYSQL_USER}'@'%';
+EOF
+
 echo "Initialization of database '${MYSQL_DATABASE}' on host '${MYSQL_HOST}' complete."
