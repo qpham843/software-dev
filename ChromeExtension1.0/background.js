d3ad2394a34a1b19dc78eb7af4459e03620017a8
Andrew Chen <chen_andrew@berkeley.edu>
2020-04-12 16:06:23 -0700

    Start refactor to divide logic among 3 javascript files

--- ChromeExtension1.0/background.js
+++ ChromeExtension1.0/background.js
@@ -1,29 +1,19 @@
 
-import { verifyAudit } from '/verify.js';
-
 /**
- * Change the icon based on verifyAudit using the URL from the current tab.
+ * Sets the chrome icon to green if vetted, default otherwise.
+ * 
+ * @param {boolean} audited Whether or the current tab has been vetted.
  */
-function changeIcon() {
-    chrome.tabs.query({active: true}, function(tab){
-			alert(tab[0].url);
-			verifyAudit(tab[0].url, (audited) => {
-				if (audited) {
-					chrome.browserAction.setIcon({path: 'pe128 - green.png'});
-				} else {
-					chrome.browserAction.setIcon({path: 'pe128.png'});				
-				}
-			});
-    });
+function changeIcon(audited) {
+	if (audited) {
+		chrome.browserAction.setIcon({path: 'pe128 - green.png'});
+	} else {
+		chrome.browserAction.setIcon({path: 'pe128.png'});				
+	}
 }
 
-//listen for new tab to be activated
-chrome.tabs.onActivated.addListener(function(activeInfo) {
-    changeIcon();
+chrome.runtime.onMessage.addListener((request) => {
+	if (typeof request.audited === 'boolean') {
+		changeIcon(request.audited);
+	}
 });
-
-//listen for current tab to be changed
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    changeIcon();
-});
-
--- ChromeExtension1.0/popup.js
+++ ChromeExtension1.0/popup.js
@@ -1,45 +1,33 @@
-import { verifyAudit } from '/verify.js';
-
-document.addEventListener('DOMContentLoaded', function() {
-    var capture = document.getElementById('captureButton');
-    //Below is the event listener that waits for you to click the 'captureButton'
-    capture.onclick = function() {
-        if (!capture.disabled) {
-          submitURL(getURL());
-        }
-    };
-});
-
 /**
  * Gets the URL previewed.
  * 
  * @return String of the URL
  */
 function getURL() {
-  return document.getElementById("websiteURL").value;
+	return document.getElementById("websiteURL").value;
 }
 
 /** 
- * Check if the current URL is in the database of article URLs.
+ * Check if the current URL is in the database of article URLs when submitting.
  * Used by XMLHttpRequest.onreadystatechange.
  */ 
 function checkURL() {
-  //Note: this.responseText is generated before this function is called. The responseText is what our server responds to the request with, but converted into a string.
-  if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("result").innerText = "This article has already been submitted.";
-  } else if (this.readyState == 4 && this.status == 500) {
-    let response = JSON.parse(this.responseText);
-    if (response.error.localeCompare("Internal Server Error") === 0 
-        && response.message.localeCompare("No message available") === 0
-        && response.path.localeCompare("/demo-0.0.1-SNAPSHOT/article/submit") === 0) {
-          //When submits for the first time, error 500 with specific message
-      document.getElementById("result").innerText = "Thank you for submitting this article!";  
-    }
-  } else if (this.readyState == 4) {
-    document.getElementById("result").innerText = "Error : " + this.responseText;
-  } else {
-    document.getElementById("result").innerText = "Loading...";
-  }
+	//Note: this.responseText is generated before this function is called. The responseText is what our server responds to the request with, but converted into a string.
+	if (this.readyState == 4 && this.status == 200) {
+		document.getElementById("result").innerText = "This article has already been submitted.";
+	} else if (this.readyState == 4 && this.status == 500) {
+		let response = JSON.parse(this.responseText);
+		if (response.error.localeCompare("Internal Server Error") === 0 
+			&& response.message.localeCompare("No message available") === 0
+			&& response.path.localeCompare("/demo-0.0.1-SNAPSHOT/article/submit") === 0) {
+			//When submits for the first time, error 500 with specific message
+		document.getElementById("result").innerText = "Thank you for submitting this article!";  
+		}
+	} else if (this.readyState == 4) {
+		document.getElementById("result").innerText = "Error : " + this.responseText;
+	} else {
+		document.getElementById("result").innerText = "Loading...";
+	}
 }
 
 /**
@@ -48,15 +36,15 @@ function checkURL() {
  * @param {String} targetURL URL to be submitted.
  */
 function submitURL(targetURL) {
-  //Generate a new request.
-  var xhttp = new XMLHttpRequest();
-  //Do the following function when ready.
-  xhttp.onreadystatechange = checkURL;
-  //The first part of the queried URL allows us to make Cross Origin HTTPS requests,
-  //The second part of the URL submits the URL to the database.
-  xhttp.open("POST", "http://157.230.221.241:8080/demo-0.0.1-SNAPSHOT/article/submit?url=" 
-    + targetURL.toString(), true);
-  xhttp.send();
+	//Generate a new request.
+	var xhttp = new XMLHttpRequest();
+	//Do the following function when ready.
+	xhttp.onreadystatechange = checkURL;
+	//The first part of the queried URL allows us to make Cross Origin HTTPS requests,
+	//The second part of the URL submits the URL to the database.
+	xhttp.open("POST", "http://157.230.221.241:8080/demo-0.0.1-SNAPSHOT/article/submit?url=" 
+		+ targetURL.toString(), true);
+	xhttp.send();
 }
 
 /**
@@ -65,9 +53,7 @@ function submitURL(targetURL) {
  * @param {String} url The URL to be previewed.
  */
 function setPreviewURL(url) {
-  document.getElementById("websiteURL").defaultValue = url;
-  verifyAudit(url, indicateAudited);
-
+	document.getElementById("websiteURL").defaultValue = url;
 }
 
 /**
@@ -78,23 +64,34 @@ function setPreviewURL(url) {
  * @param {boolean} audited Whether or not an article has already been submitted and audited.
  */
 function indicateAudited(audited) {
-  console.log(audited);
-  if (audited) {
-  
-    document.getElementById("captureButton").disabled = true;
-    document.getElementById("result").innerText = "This article has already been submitted.";
-  } else {
-    //TODO: visualize when not audited. This causes errors in the if true case.
-    // document.getElementById("captureButton").disabled = false;
-    // document.getElementById("result").innerText = "";
-  }
+	if (audited) {	
+		document.getElementById("captureButton").disabled = true;
+		document.getElementById("result").innerText = "This article has already been submitted.";
+	}
 }
 
+document.addEventListener('DOMContentLoaded', () => {
+    const capture = document.getElementById('captureButton');
+    capture.onclick = () => {
+        if (!capture.disabled) {
+          submitURL(getURL());
+        }
+	};
+	/**
+	 * From background.js, we can determine whether the current tab has been vetted or not.
+	 * First, a message is sent to background.js to tell background.js that popup.js has opened.
+	 * Second, background script checks whether the current tab has been audited.
+	 * Third, pop.js receives the results of that check.
+	 */
+	chrome.tabs.query({active: true}, tabs => {
+		chrome.runtime.sendMessage(tabs[0].id, {});
+	});
+});
 
 /** Sets default value to current tab's URL. */
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-  //Gets the first tab and it's url.
-  setPreviewURL(tabs[0].url);
+	setPreviewURL(tabs[0].url);
 });
-
-
+chrome.runtime.onMessage.addListener((request) => {
+	indicateAudited(request.audited);
+});
