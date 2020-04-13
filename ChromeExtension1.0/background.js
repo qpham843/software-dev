eb4cbb485791673e7b5d7e98b53d4403c564657d
Andrew Chen <chen_andrew@berkeley.edu>
2020-04-13 11:15:51 -0700

    Clean up code and images organization, and update readme

--- ChromeExtension1.0/background.js
+++ ChromeExtension1.0/background.js
@@ -9,25 +9,35 @@ function changeIcon(audited) {
 	if (audited) {
 		chrome.browserAction.setIcon({
 			path: {
-				"48": "pe48 - green.png",
-				"128": "pe128 - green.png"
+				"48": "images/pe48 - green.png",
+				"128": "images/pe128 - green.png"
 			}
 		});
 	} else {
 		chrome.browserAction.setIcon({
 			path: {
-				"48": "pe48.png",
-				"128": "pe128.png"
+				"48": "images/pe48.png",
+				"128": "images/pe128.png"
 			}
 		});
 	}
 }
 
-/** Use content script to detect tab change. On tab changes, use verifyAudit on the new url.  */
-chrome.runtime.onMessage.addListener((request) => {
-	if (request.from === "content") {
-		chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-			verifyAudit(tabs[0].url, changeIcon);
-		});
-	}
+/** On tab changes, reverify. */
+chrome.tabs.onUpdated.addListener(() => {
+	chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+		verifyAudit(tabs[0].url, changeIcon);
+	});
+});
+
+chrome.tabs.onCreated.addListener(() => {         
+	chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+		verifyAudit(tabs[0].url, changeIcon);
+	});
+});
+
+chrome.tabs.onActivated.addListener(() => {         
+	chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+		verifyAudit(tabs[0].url, changeIcon);
+	});
 });
