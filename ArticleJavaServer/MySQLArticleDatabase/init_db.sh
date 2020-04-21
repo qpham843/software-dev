0b048710f5d2d40aeefa89c33235843dc1af9a2b
Norman Gilmore <git@teamforecast.com>
2020-04-20 19:52:07 -0700

    Also need to delete a-users.sql references from init_db.sh.

--- ArticleJavaServer/MySQLArticleDatabase/init_db.sh
+++ ArticleJavaServer/MySQLArticleDatabase/init_db.sh
@@ -7,8 +7,6 @@ user = ${MYSQL_ROOT_USER}
 password = ${MYSQL_ROOT_PASSWORD}
 EOF
 chmod 0600 my.cnf
-echo "Initializing database '${MYSQL_DATABASE}' on host '${MYSQL_HOST}' with a-users.sql"
-mysql --defaults-extra-file=my.cnf -h ${MYSQL_HOST} ${MYSQL_DATABASE} < /docker-entrypoint-initdb.d/a-users.sql
 echo "Initializing database '${MYSQL_DATABASE}' on host '${MYSQL_HOST}' with publiceditor-database-dump.sql"
 mysql --defaults-extra-file=my.cnf -h ${MYSQL_HOST} ${MYSQL_DATABASE} < /docker-entrypoint-initdb.d/publiceditor-database-dump.sql
 
