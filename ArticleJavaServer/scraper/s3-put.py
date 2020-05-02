784b9473b89ffa61b5a2d341b5593f8d9de0cfcf
James McKay <technolefty@gmail.com>
2020-05-02 14:32:12 -0400

    DEV flag value and s3-put display message

--- ArticleJavaServer/scraper/s3-put.py
+++ ArticleJavaServer/scraper/s3-put.py
@@ -9,7 +9,7 @@ AWS_S3_ARTICLE_PREFIX  = os.getenv('AWS_S3_ARTICLE_PREFIX', 'articles/')
 localFilePathAndName = sys.argv[1]
 filename = sys.argv[2]
 print("in s3-put.py ")
-print("sending " + localFilePathAndName + filename)
+print("sending " + localFilePathAndName)
 print ("to " + AWS_S3_ARTICLE_PREFIX + filename)
 
 result = boto3.client('s3').upload_file(localFilePathAndName, AWS_S3_ARTICLE_BUCKET, AWS_S3_ARTICLE_PREFIX + filename)
