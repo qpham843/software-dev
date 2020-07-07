62f941ac4a9825c342c9a75f2afc4110252ad4a7
Chris Apton <chrisapton@Chriss-MacBook-Pro.local>
2020-07-07 11:30:41 -0700

    add, delete, and get tags added to manage tags

--- peclient/src/app/manage-tags/manage-tags.component.ts
+++ peclient/src/app/manage-tags/manage-tags.component.ts
@@ -10,22 +10,41 @@ export class ManageTagsComponent implements OnInit {
 	{ 
 	}
 
-  ngOnInit() {
-	this.ts.getTags().subscribe((data: tag) => {
-		console.log(data);
-		this.tags = data;
-		console.log(this.tags);
-		console.log(this.tags.length);
-		for(let x = 0; x < this.tags.length; x++) {
-			if(this.tags[x] != undefined)
-			{
-			console.log(this.tags[x].tag);
+  	ngOnInit() {
+		this.ts.getTags().subscribe((data: tag) => {
+			console.log(data);
+			this.tags = data;
+			console.log(this.tags);
+			console.log(this.tags.length);
+			for(let x = 0; x < this.tags.length; x++) {
+				if(this.tags[x] != undefined)
+				{
+					console.log(this.tags[x].tag);
+				}
 			}
-		}
-  	});
+  		});
+  	}
+
+  	addTags(tag:string)
+  	{
+  		this.ts.addTag(tag);
+  	}
+
+  	deleteTags() {
+  		console.log("deleting tags");
+	 	let checkboxes = document.getElementsByName("articleCheckbox");
+	  	checkboxes.forEach(cb => {
+	  		let cbe = cb as HTMLInputElement;
+  			if (cbe.checked) 
+  			{ 
+  				console.log(cbe.value)
+  				for(let x = 0; x < this.tags.length; x++) {
+					if(this.tags[x] != undefined && this.tags[x].tag == cbe.value)
+					{
+						this.ts.deleteTag(this.tags[x].id);
+					}
+				}
+  			}
+	  	})
   }
-  addTag(tag:string)
-  {
-  console.log(tag);
-  }
- }
+}
--- peclient/src/app/manage-tags/manage-tags.service.ts
+++ peclient/src/app/manage-tags/manage-tags.service.ts
@@ -26,6 +26,7 @@ export class TagService {
   }
 
   addTag(name: string) {
+    console.log("Tag added");
     return this.http.post(this.apiDest + '/tags?name=' + name, null);
   }
 
