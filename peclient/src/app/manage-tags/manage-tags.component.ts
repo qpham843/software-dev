f767c63be093765e0893a8fbae404f89467e1f21
Chris Apton <chrisapton@Chriss-MacBook-Pro.local>
2020-07-07 10:34:16 -0700

    minor changes to tags

--- peclient/src/app/manage-tags/manage-tags.component.ts
+++ peclient/src/app/manage-tags/manage-tags.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import {TagService} from './manage-tags.service';
 @Component({
   selector: 'app-manage-tags',
   templateUrl: './manage-tags.component.html',
   styleUrls: ['./manage-tags.component.css']
 })
 export class ManageTagsComponent implements OnInit {
-
-  constructor() { }
+	constructor(private ts: TagService)
+	{ 
+	}
 
   ngOnInit() {
+	this.ts.getTags().subscribe((data: tag) => {
+		console.log(data);
+		this.tags = data;
+		console.log(this.tags);
+		console.log(this.tags.length);
+		for(let x = 0; x < this.tags.length; x++) {
+			if(this.tags[x] != undefined)
+			{
+			console.log(this.tags[x].tag);
+			}
+		}
+  	});
   }
-
-}
+  addTag(tag:string)
+  {
+  console.log(tag);
+  }
+ }
