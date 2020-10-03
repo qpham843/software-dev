102d579c04c1ba825b2fbff4949869178205beb7
James McKay <technolefty@gmail.com>
2020-10-03 14:27:13 -0400

    allow return of 75 covid2 articles instead of 25

--- ArticleJavaServer/MySQLArticleDatabase/publiceditor-database-dump.sql
+++ ArticleJavaServer/MySQLArticleDatabase/publiceditor-database-dump.sql
@@ -472,3 +472,8 @@ update buzz_query set filename_tag = 'Covid2EvergreenArticles' where filename_ta
 
 delete from article where filename_tag like '%evergreen%' and publish_date > timestamp('2020-05-05');
 update article set filename_tag = 'Covid2EvergreenArticles' where filename_tag like '%evergreen%';
+
+insert into buzz_query 
+(id, query, filename_tag, active_flag) 
+values 
+(6, 'https://api.buzzsumo.com/search/trends.json?topic=coronavirus,covid&search_type=trending_now&hours=24&count=75&countries=United States', 'Covid2Articles',true);
